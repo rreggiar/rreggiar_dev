78c954c5476e86146c3b078c5304d7ed057d95c1
rreggiar <rreggiar@ucsc.edu>
2023-09-17 12:19:37 -0700

    adding optional installs

--- singularity/R/install_PERL.sh
+++ singularity/R/install_PERL.sh
@@ -2,4 +2,4 @@
 # cpan -u
 
 apt-get remove -y perl-homedir
-cpan install File::Which HTML::Template JSON XML::Parser Sys::Info
+cpan install File::Which HTML::Template JSON XML::Parser Sys::Info Log::Log4perl Math::CDF XML::Compile::SOAP11 XML::Compile::WSDL11
