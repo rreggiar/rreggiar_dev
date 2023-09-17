52047756e38e116744c4bad0e3228a47b3b211ca
rreggiar <rreggiar@ucsc.edu>
2023-09-17 12:21:33 -0700

    adding build_essential

--- singularity/R/install_PERL.sh
+++ singularity/R/install_PERL.sh
@@ -1,5 +1,5 @@
 # neccessary PERL dependencies to install MEME
 # cpan -u
 
-apt-get remove -y perl-homedir
+apt-get install -y build-essential
 cpan install File::Which HTML::Template JSON XML::Parser Sys::Info Log::Log4perl Math::CDF XML::Compile::SOAP11 XML::Compile::WSDL11
