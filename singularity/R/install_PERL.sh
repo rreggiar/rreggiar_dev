fed11638785c6d159b2ad7ed9993661bd298b53a
rreggiar <rreggiar@ucsc.edu>
2023-09-18 09:53:32 -0700

    trying to install perl mods with apt-get

--- singularity/R/install_PERL.sh
+++ singularity/R/install_PERL.sh
@@ -1,5 +1,5 @@
 # neccessary PERL dependencies to install MEME
 # cpan -u
 
-apt-get install -y build-essential
-cpan install File::Which HTML::Template JSON XML::Parser Sys::Info Log::Log4perl Math::CDF XML::Compile::SOAP11 XML::Compile::WSDL11
+apt-get install -y build-essential libfile-which-perl libhtml-template-perl libjson-perl libxml-parser-perl libsys-info-perl liblog-log4perl-perl libmath-cdf-perl libxml-compile-perl
+#cpan install File::Which HTML::Template JSON XML::Parser Sys::Info Log::Log4perl Math::CDF XML::Compile::SOAP11 XML::Compile::WSDL11
